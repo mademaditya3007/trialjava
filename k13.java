de48b3b98ee768d44da949900706dbe1d4ae597a
mademaditya3007 <mademaditya3007@gmail.com>
2023-04-28 23:23:18 +0530

    first

---
+++ k13.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class k13 {
+    public static void main(String[] args){
+        Scanner k1=new Scanner(System.in);
+        int a=k1.nextInt();
+        int b=k1.nextInt();
+        System.out.println(a+b);
+
+    }
+}
